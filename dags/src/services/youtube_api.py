9d041d3078a318fe5fa44e774b8a99427aea1e65
rodrigorocha1 <silva.rodrigo31@outlook.com>
2025-03-09 14:55:16 -0300

    método pronto para buscar assunto

--- dags/src/services/youtube_api.py
+++ dags/src/services/youtube_api.py
@@ -1,1 +1,40 @@
+from typing import Any, Dict, Generator
+from googleapiclient.discovery import build
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+
 class YoutubeAPI:
+    def __init__(self):
+        self.__CHAVE = os.environ['CHAVE_API']
+        self.__youtube = build('youtube', 'v3', developerKey=self.__CHAVE)
+
+    def buscar_assunto(self, assunto: str, data_publicacao: str) -> Generator[Dict[str, Any], None, None]:
+        """Método para buscar o assunto
+
+        Args:
+            assunto (str): assunto do vídeo
+            data_publicacao (str): data de públicação
+
+        Yields:
+            Generator[Dict[str, Any], None, None]: Gerador com os vídeos
+        """
+        next_page_token = None
+        while True:
+            request = self.__youtube.search().list(
+                part='snippet',
+                q=assunto,
+                publishedAfter=data_publicacao,  # Formato ISO 8601
+                order='date',
+                pageToken=next_page_token
+
+            )
+
+            response = request.execute()
+
+            yield from response['items']
+            next_page_token = response['nextPageToken']
+            print(next_page_token)
+            if not next_page_token:
+                break
